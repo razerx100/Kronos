4ea235c8f0eca00902ef42155b10adb40fe093a1
Saikat <saikatmandal8@gmail.com>
2020-10-20 21:32:28 +0600

    Windows setup script

--- Setup.bash
+++ Setup.bash
@@ -1,2 +1,3 @@
+mkdir -p build
 cd build
 cmake .. -DPLATFORM:STRING=Linux -DCMAKE_BUILD_TYPE=Debug
---
+++ Setup.bat
@@ -0,0 +1,4 @@
+rmdir /s /q .\build
+mkdir .\build
+cd .\build
+cmake .. -DPLATFORM:STRING=Windows
